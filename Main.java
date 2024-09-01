e87b9c7bfa31a23294e29c7898c5e50ae547b80f
Nikhil-Kurakula <nikhilkurakula16@gmail.com>
2024-09-01 22:21:54 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BankAccount account = new BankAccount(500); 
-
-        ATM atm = new ATM(account);
-
-        atm.displayMenu();
-    }
-}
